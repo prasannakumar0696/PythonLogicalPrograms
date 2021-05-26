c21158653ab8842ec5ab7982b6420412e2832023
prasannakumar0696 <61589507+prasannakumar0696@users.noreply.github.com>
2021-05-26 19:01:11 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,10 @@
+input_list = [1, 2, 3, 4, 4, 5, 6, 7, 7]
+
+list_using_comp = [var for var in input_list if var % 2 == 0 ]
+
+print("Output List using list comprehensions:",
+      list_using_comp)
+languages = ["Python", "C Programming", "Java", "JavaScript"]
+smallest_string = min(languages);
+
+print("The smallest string is:", smallest_string)
